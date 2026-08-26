c10a7f488f5b25043361b52feada24d3f0a9cd10
Steven Brom <steven.brom@outlook.com>
2026-08-26 02:05:44 +0200

    First commit of tab control

---
+++ tab-control/tab-control.js
@@ -0,0 +1,197 @@
+customElements.define("tab-control", class TabControl extends HTMLElement {
+    tabPanels = [];
+    tabButtons = [];
+    
+    constructor(){
+        super();
+    
+        this.onMutation = this.onMutation.bind(this);
+        this.tabsAdded = false;
+    }
+    
+    onMutation(mutations) {
+        if (this.tabsAdded) return;
+        
+        const newElements = [];
+    
+        // A `mutation` is passed for each new node
+        for (const mutation of mutations) {
+          // Could test for `mutation.type` here, but since we only have
+          // set up one observer type it will always be `childList`
+          newElements.push(...mutation.addedNodes);
+        }
+        this.tabPanels = newElements.filter(el => el.nodeType === Node.ELEMENT_NODE && el.className == "tab-panel");
+        
+        this.tabsAdded = true;
+        
+        let tabPanelContainer = document.createElement("div");
+        tabPanelContainer.classList.add("tab-control");
+        this.appendChild(tabPanelContainer);
+        
+        let tabstripElement = document.createElement("div");
+        tabstripElement.className = "tab-strip";
+        
+        if (this.tabstripLocation == "top") 
+            tabPanelContainer.appendChild(tabstripElement);
+        
+        let tabPanelsContainer = document.createElement("div");
+        tabPanelsContainer.classList.add("tab-panels");
+        tabPanelContainer.appendChild(tabPanelsContainer);
+        
+        this.tabPanels.forEach(tabPanel => {
+            tabPanel.style.display = "none";
+            tabPanel.classList.add("tab-panel");
+            tabPanelsContainer.appendChild(tabPanel);
+        });
+        
+        if (this.tabstripLocation == "bottom") 
+            tabPanelContainer.appendChild(tabstripElement);
+        
+        this.tabPanels.forEach(tabPanel => {
+            let tabButton = document.createElement("button");
+            tabButton.className = "tab-button";
+            tabButton.innerText = tabPanel.getAttribute("name");
+            tabstripElement.appendChild(tabButton);
+            this.tabButtons.push(tabButton);
+            tabButton.onclick = () => this.clickTab(tabButton);
+        });
+      }
+      
+    defineprop(computedStyle){
+        const plist=this.module.properties;
+        for(let k in plist){
+            const v = plist[k];
+            let value = v.value;
+            if (!value) {
+                if (computedStyle[k]) {
+                    value = computedStyle[k];
+                } else if (v.defaultValue) {
+                    value = v.defaultValue;
+                }
+            }
+            this["_"+k] = this.getAttr(k,value);
+            
+            Object.defineProperty(this, k, {
+                get:()=>{return this["_"+k]},
+                set:(val)=>{
+                    this["_"+k] = val;
+                    if(typeof(this[v.observer])=="function")
+                        this[v.observer]();
+                }
+            });
+        }        
+    }
+    
+    connectedCallback(){
+        let root;
+        root=this;
+        
+        this.module = {
+            is:"tab-control",
+            properties:{
+                color:                      {type:String, value:"",     defaultValue:"#000000"},
+                backgroundColor:            {type:String, value:"#e0e0e0",     defaultValue:"#e0e0e0"},
+                tabstripBackgroundColor:    {type:String, value:"",     defaultValue:"#808080"},
+                inactiveTabBackgroundColor: {type:String, value:"",     defaultValue:"#b0b0b0"},
+                activeTabBackgroundColor:   {type:String, value:"",     defaultValue:"#d0d0d0"},
+                tabstripLocation:           {type:String, value:"top"},
+                height:                     {type:String, value:"",     defaultValue:"400px"},
+                'class':                    {type:String, value:"tab-control"}
+            },
+        };
+        let computedStyle = window.getComputedStyle(this);
+        this.defineprop(computedStyle);
+        
+        let css = `<style>
+.tab-control {
+  background-color: ${this.backgroundColor};
+  border: 1pt solid rgba(0,0,0,0.5);
+  height: ${this.height};
+  
+  .tab-strip {
+    margin: 0px;
+    padding: 0px;
+    padding-left: 1px;
+    width: stretch;
+    background-color: ${this.tabstripBackgroundColor};
+    
+    .tab-button {
+      border: 1px solid rgba(0,0,0,0.5);
+      background-color: ${this.inactiveTabBackgroundColor};
+    }
+    
+    .tab-button.active-tab {
+      background-color: ${this.activeTabBackgroundColor};
+    }
+  }
+  
+  .tab-panels {
+    height: 400px;
+    padding: 5px;
+  }
+  
+  .tab-panel {
+    display: none;
+    height: 100%; 
+    border: 1px solid rgba(0,0,0,0.5);
+  }
+}
+:host {
+    user-select: none;
+    padding:0;
+    margin:0;
+}
+</style>`;
+        this.innerHTML = css;
+
+        // Set up observer
+        this.observer = new MutationObserver(this.onMutation);
+    
+        // Watch the Light DOM for child node changes
+        this.observer.observe(this, {
+          childList: true
+        });
+        
+        this.ready=function(){
+        };
+
+        this.clickTab=function(tabButton) {
+            console.log("clickTab")
+          // Get all elements with class="tabcontent" and hide them
+          this.tabPanels.forEach(panel => panel.style.display = "none");
+          
+          let openTab = !tabButton.classList.contains("active-tab");
+          
+          // Get all elements with class="tablinks" and remove the class "active"
+          this.tabButtons.forEach(button => button.classList.remove("active-tab"));
+        
+          if (openTab) {
+            // Show the current tab, and add an "active" class to the button that opened the tab
+            let tabPanel = this.tabPanels.find(tabPanel => tabPanel.getAttribute("name") === tabButton.innerText);
+            tabPanel.style.display = "flex";
+            tabButton.classList.add("active-tab");
+            console.log("show panel " + tabButton.innerText)
+          }
+        };
+        
+        this.ready();
+    }
+    
+    disconnectedCallback() {
+        // remove observer if element is no longer connected to DOM
+        this.observer.disconnect();
+    }
+    
+    getAttr(n, def) {
+        let v = this.getAttribute(n);
+        if (v == "" || v == null) return def;
+        switch (typeof(def)) {
+            case "number":
+                if (v == "true") return 1;
+                v = +v;
+                if (isNaN(v)) return 0;
+                return v;
+        }
+        return v;
+    }
+});
