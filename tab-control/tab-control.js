91e598d1e2f7b2b47e7233cb6017f143178a1902
Steven Brom <steven.brom@outlook.com>
2026-09-25 18:00:44 +0200

    update tab-control.js

--- tab-control/tab-control.js
+++ tab-control/tab-control.js
@@ -118,7 +118,7 @@ customElements.define("tab-control", class TabControl extends HTMLElement {
     padding: 0px;
     padding-left: 1px;
     width: stretch;
-    height: auto;
+    height: stretch;
     background-color: ${this.tabstripBackgroundColor};
     
     .tab-button {
